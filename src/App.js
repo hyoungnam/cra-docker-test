a5f3dbce7e45bf88e6f9cdbe1fb2fc796442a086
Hyoungnam Kim <kimhyoungnams@gmail.com>
2020-12-12 21:23:33 +0900

    fix: deploy & app.js message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Nginx React
+          Hello EB Docker React
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello Nginx React/i);
+  const linkElement = screen.getByText(/Hello EB Docker React/i);
   expect(linkElement).toBeInTheDocument();
 });
